e0f9c40914b73cbb872633fd003ff15bf42ed70c
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-03-25 09:58:29 -0400

    Added First Two Parts of the Union Find Case Study

---
+++ src/UnionFind/QuickUnionUF.java
@@ -0,0 +1,31 @@
+package UnionFind;
+
+public class QuickUnionUF {
+    private int[] id;
+
+    public QuickUnionUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    private int root(int i) {
+        /* Recursive Way:
+        if (id[i] == i) return i;
+        else return root(id[i]);
+         */
+        while (i != id[i]) i = id[i];
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int pRoot = root(p);
+        int qRoot = root(q);
+        id[pRoot] = qRoot;
+    }
+}
