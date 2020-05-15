01a6f092b915ea3a6469686bb775f29f7c1dcdbf
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-15 09:25:24 -0400

    Added Resizing Array Stack

---
+++ src/StacksAndQueues/ResizingArrayStackOfStrings.java
@@ -0,0 +1,30 @@
+package StacksAndQueues;
+
+public class ResizingArrayStackOfStrings {
+    private String[] s;
+    private int N = 0;
+    public ResizingArrayStackOfStrings() {
+        s = new String[1];
+    }
+    
+    public void push(String item) {
+        if (N == s.length) resize(2 * s.length);
+        s[N++] = item;
+    }
+    
+    private void resize(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < N; i++) {
+            copy[i] = s[i];
+        }
+        s = copy;
+    }
+    
+    public String pop() {
+        String item = s[--N];
+        s[N] = null;
+        //Prevent Thrashing by resizing array to 1/2 size when it is 1/4 full
+        if (N > 0 && N == s.length/4) resize(s.length/2);
+        return item;
+    }
+}
