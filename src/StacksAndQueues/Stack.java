bb1acb6e7eb4216716b505ce64ea5b2b17f44db1
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-19 20:14:43 -0400

    Added Generic Stack Implementation

---
+++ src/StacksAndQueues/Stack.java
@@ -0,0 +1,27 @@
+package StacksAndQueues;
+
+public class Stack<T> {
+    private Node first = null;
+    
+    private class Node {
+        T    item;
+        Node next;
+    }
+    
+    public boolean isEmpty() {
+        return first == null;
+    }
+    
+    public void push(T item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+    }
+    
+    public T pop() {
+        T item = first.item;
+        first = first.next;
+        return item;
+    }
+}
