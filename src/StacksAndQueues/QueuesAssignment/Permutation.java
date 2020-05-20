9f4f0a1429312d805ed0a2190ef7061db29fe401
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-20 15:22:50 -0400

    Created First Version of Permutation

---
+++ src/StacksAndQueues/QueuesAssignment/Permutation.java
@@ -0,0 +1,20 @@
+package StacksAndQueues.QueuesAssignment;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+    public static void main(String[] args) {
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        
+        int numRand = Integer.parseInt(args[0]);
+        
+        while (!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
+        }
+        
+        for (int i = 0; i < numRand; i++) {
+            StdOut.println(rq.dequeue());
+        }
+    }
+}
