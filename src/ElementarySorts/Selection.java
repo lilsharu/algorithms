1f92fe698b2e3a1231fbc017f94877f359994f8b
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-21 09:10:18 -0400

    Added Selection Sort

---
+++ src/ElementarySorts/Selection.java
@@ -0,0 +1,24 @@
+package ElementarySorts;
+
+public class Selection {
+    public static void sort(Comparable[] arr) {
+        int N = arr.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(arr[j], arr[min])) min = j;
+            }
+            exch(arr, i, min);
+        }
+    }
+    
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
